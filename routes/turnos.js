06896f4a8806f821a6a48d8109e5029441650e41
Pablo Bertini Luque <98782643+Pablo-Bertini-Luque@users.noreply.github.com>
2023-12-31 20:55:18 -0300

    Modificacion-categoría-turnos

--- routes/turnos.js
+++ routes/turnos.js
@@ -24,9 +24,7 @@ router.post(
     check("hora", "Debe indicar la hora de inicio y fin del turno")
       .not()
       .isEmpty(),
-    check("categoria", "Debe ingresar un número del 1 al 8").isIn([
-      1, 2, 3, 4, 5, 6, 7, 8,
-    ]),
+    check("categoria", "Debe ingresar un número del 1 al 8").isNumeric(),
     check("tipoCancha", "Indicar si la cancha es de cemento o cesped").isIn([
       "CESPED",
       "CEMENTO",
